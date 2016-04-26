b176a7179b9ade944c0612f33102093def8eaa47
Philippe Akira <Philippe Akira>
2016-04-25 22:36:20 -0300

    Initial commit

---
+++ src/Bigode.java
@@ -0,0 +1,5 @@
+public class Bigode {
+
+    public static void main(String[] args){
+    }
+}
---
+++ src/com/bigode/utils/JDBCConnection.java
@@ -0,0 +1,62 @@
+package com.bigode.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConnection {
+    private final String connUrl = "jdbc:mysql://localhost:3306/BIGODE";
+    private final String username = "root";
+    private final String password = "EACHesi2016!";
+    private static JDBCConnection jdbcInstance = null;
+    private Connection connection = null;
+
+    private JDBCConnection(){}
+
+    public static JDBCConnection getJdbcInstance() {
+        if(jdbcInstance == null){
+            jdbcInstance = new JDBCConnection();
+        }
+
+        return jdbcInstance;
+    }
+
+    public Connection connect(){
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(connUrl, username, password);
+        }
+        catch (SQLException jdbcEx){
+            jdbcEx.printStackTrace();
+        }
+        catch (ClassNotFoundException classEx){
+            classEx.printStackTrace();
+        }
+        finally {
+            try {
+                connection.close();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        return connection;
+    }
+
+    public void close() {
+
+        try {
+            connection.close();
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        } finally {
+            connection = null;
+        }
+    }
+
+    public Boolean isConnected(){
+        return (connection != null);
+    }
+}
