8298129c3a8aead0ceff16e9f12532628530aa85
Phil <p_akira@live.com>
2016-04-28 00:00:00 -0300

    Attempt to fix maven build on production server

--- src/main/java/com/bigode/utils/BigodeServlet.java
+++ src/main/java/com/bigode/utils/BigodeServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/bigode-dono")
+@WebServlet("/api/dono/v1/")
 public class BigodeServlet extends HttpServlet{
 
     public BigodeServlet(){
